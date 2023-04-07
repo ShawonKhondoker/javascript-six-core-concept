d38c1d2cf42bec9fa9bebc0a1a5dafe0f02e27ab
Shawon Khondoker <shawonkhondoker0.com>
2023-04-07 03:16:34 -0700

    first commit

---
+++ varible.js
@@ -0,0 +1,148 @@
+// //.........variable declare.....
+// let myVariable = 'Hello world';
+// const PI = 3.14159;
+// var x = 5;
+// console.log(x);
+
+// //....String ,Bullian ,Numceric...
+
+// let MyString = "Hello ,World!";
+
+// let Istrue = true;
+
+// let MyNumber = 42 ;
+
+// console.log(MyNumber);
+
+// //........Addition....
+
+// let num1 = 10 ;
+// let num2 = 5 ;
+// let sum = num1 + num2;
+// console.log (sum);
+
+// //........Subtraction....
+
+// let num3 = 10 ;
+// let num4 = 5 ;
+// let sum1 = num3 - num4;
+// console.log (sum1);
+
+// //........Multiplication....
+
+// let num5 = 10 ;
+// let num6 = 5 ;
+// let sum3 = num5 * num6;
+// console.log (sum3);
+
+// //........Division....
+
+// let num7 = 10 ;
+// let num8 = 5 ;
+// let sum4 = num7 / num8;
+// console.log (sum4);
+
+// //........Modulo....
+
+// let num9 = 10 ;
+// let num10 = 3 ;
+// let sum5 = num9 % num10;
+// console.log (sum5);
+
+
+// //...............Condition (Comparison ,if else)......
+// let num1 = 10;
+// let num2 = 5;
+
+// // Equality operator (==)
+// if (num1 == num2) {
+//   console.log("num1 is equal to num2");
+// } else {
+//   console.log("num1 is not equal to num2");
+// }
+
+// // Greater than operator (>)
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else {
+//   console.log("num1 is not greater than num2");
+// }
+
+// // Less than operator (<)
+// if (num1 < num2) {
+//   console.log("num1 is less than num2");
+// } else {
+//   console.log("num1 is not less than num2");
+// }
+
+// // Greater than or equal to operator (>=)
+// if (num1 >= num2) {
+//   console.log("num1 is greater than or equal to num2");
+// } else {
+//   console.log("num1 is not greater than or equal to num2");
+// }
+
+// // Less than or equal to operator (<=)
+// if (num1 <= num2) {
+//   console.log("num1 is less than or equal to num2");
+// } else {
+//   console.log("num1 is not less than or equal to num2");
+// }
+
+// // Inequality operator (!=)
+// if (num1 != num2) {
+//     console.log("num1 is not equal to num2");
+//     } else {
+//     console.log("num1 is equal to num2");
+//     }
+
+
+// //............if-else........
+
+
+// let num = 10;
+// if (num > 0){
+//     console.log("num is Positive")
+//     } else if(num < 0){
+//         console.log("num is negative");
+//     } else{
+//         console.log("num is zero")
+//     }
+
+
+//     let num01 = 10;
+//     let num02 = 5;
+    
+//     if (num01 > 0) {
+//       if (num02 > 0) {
+//         console.log("num1 and num2 are both positive");
+//       } else {
+//         console.log("num1 is positive but num2 is not");
+//       }
+//     } else {
+//       console.log("num1 is not positive");
+//     }
+
+
+    let num1 = 10;
+let num2 = 5;
+
+// Greater than operator
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+}
+
+// Less than operator
+if (num2 < num1) {
+  console.log("num2 is less than num1");
+}
+
+// Equal to operator
+if (num1 == 10) {
+  console.log("num1 is equal to 10");
+}
+
+// Not equal to operator
+if (num2 != 10) {
+  console.log("num2 is not equal to 10");
+}
