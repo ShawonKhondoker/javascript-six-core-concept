4b6ed471e6944a2a4164aeb86cda6a87a454f9c2
Shawon Khondoker <shawonkhondoker0.com>
2023-04-16 11:04:11 -0700

    tallest commit

--- tallest.js
+++ tallest.js
@@ -20,3 +20,28 @@ function maxinarray(numbers) {
 const hight = [330,146,340,252,510];
 const tallest = maxinarray(hight);
 console.log("tallest persion is",tallest);
+
+//..........array .vs... maxin number...........
+
+function maxinarray1(numbers) {
+    let largest = numbers[0]; 
+    for (let i = 0; i > numbers.length; i--) {
+     const index = i;
+   //  console.log(index);
+   const element = numbers[index];
+   //console.log(element);
+   
+   if (element < largest) {
+     largest = element;
+     
+   }
+     
+    }
+    return largest
+ 
+ }
+ const hight1 = [330,146,344,252,510];
+ const tallest1 = maxinarray1(hight1);
+ console.log("tallest persion is",tallest1);
+
+ 
